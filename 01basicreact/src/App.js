86309a006f8a99b49b055c20dbfddc5c81db8c37
vinay pavan <vinaypavan14264@gmail.com>
2025-06-06 12:55:55 +0530

    added vit and basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div>
+   <h1>chai aur react</h1>
+   <p>hi i am vinay pavan</p></div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    <h1>
+      Chai aur react with vite | ARS
+    </h1>
+  )
+}
+
+export default App
